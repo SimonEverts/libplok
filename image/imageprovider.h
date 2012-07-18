d538cbcefdb75cbb717759f14f62893518f1e7ac
S Everts <simoneverts@gmail.com>
2012-07-18 22:34:17 +0200

    added imageprovider

---
+++ image/imageprovider.h
@@ -0,0 +1,37 @@
+#ifndef IMAGEPROVIDER_H
+#define IMAGEPROVIDER_H
+
+//#include <QDeclarativeImageProvider>
+#include <QImage>
+
+#include "image.h"
+
+class ImageReader;
+//class ExifLoader;
+
+class ImageProvider
+{
+public:
+    explicit ImageProvider(QObject *parent = 0);
+    virtual ~ImageProvider (void);
+
+    QImage requestThumbnail( const QString& id, QSize* size, const QSize& requestedSize );
+
+    Image loadThumbnail (QString fileName);
+    Image loadPreview (QString fileName);
+    Image loadMaster (QString fileName);
+
+    ImageReader* imageReaderFromFormat (QString format);
+
+    QStringList supportedSuffixes (void);
+
+private:
+    QList <ImageReader*> m_imageReaders;
+
+//    ExifLoader* m_exifLoader;
+
+    void correctOrientation (QString fileName, QImage& thumb);
+    void correctOrientation (QString fileName, Image& thumb);
+};
+
+#endif // IMAGEPROVIDER_H
--- libplok.pro
+++ libplok.pro
@@ -12,8 +12,10 @@ DEFINES += LIBPLOK_LIBRARY
 SOURCES += 	image/image.cpp \
                 image/imagereader.cpp \
                 image/imagereader_qt.cpp \
+                image/imageprovider.cpp
 
 HEADERS += 	image/image.h \
                 image/imagereader.h \
-                image/imagereader_qt.h
+                image/imagereader_qt.h \
+                image/imageprovider.h
 
